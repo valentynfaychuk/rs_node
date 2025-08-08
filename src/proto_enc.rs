f6a8506e240c3de616780ab02121371ac1e2c16c
lissana <ktion23@gmail.com>
2025-08-08 11:40:21 +0800

    .

--- src/proto_enc.rs
+++ src/proto_enc.rs
@@ -141,6 +141,7 @@ fn parse_entry_summary(term: Option<&Term>) -> Result<EntrySummary, ParseError> 
         .get(&Term::Atom(Atom::from("mask")))
         .and_then(|t| t.binary())
         .map(|b| b.to_vec());
+
     Ok(EntrySummary {
         header: header.to_vec(),
         signature: signature.to_vec(),
